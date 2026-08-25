92f25bc38c24dd08919729158d6b6f687513fb09
Diya Jabin <diyasjabin@gmail.com>
2026-08-25 21:11:18 +0530

    feat: add robot registration system

--- awfms_ws/src/awfms_fleet_manager/awfms_fleet_manager/fleet_manager.py
+++ awfms_ws/src/awfms_fleet_manager/awfms_fleet_manager/fleet_manager.py
@@ -3,24 +3,42 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import String 
+from awfms_interfaces.srv import RegisterRobot
 
 class FleetManager(Node):
     def __init__(self):
         super().__init__("fleet_manager") 
-        self.publisher_ = self.create_publisher(String, "/fleet_manager/status",10)
+        self.robot_registry={} #hold all the registered robots
+        self.status_publisher_ = self.create_publisher(String, "/fleet_manager/status",10)
         self.timer_ = self.create_timer(0.5, self.publish_status)
-        self.subscriber_ = self.create_subscription(String, "/robot/status",self.callback_robot_status, 10)
+        self.robot_status_subscriber_ = self.create_subscription(String, "/robot/status",self.callback_robot_status, 10)
+        self.register_service=self.create_service(RegisterRobot,"/fleet_manager/register_robot",self.register_robot_callback)
         self.get_logger().info("Fleet Manager Node has been started")
 
     def callback_robot_status(self,msg:String):
         self.get_logger().info(f"Received robot status: {msg.data}")
 
+    def register_robot_callback(self,request:RegisterRobot.Request,response:RegisterRobot.Response):
+        self.get_logger().info(f"Registration request received from: {request.robot_id}")
+        if request.robot_id in self.robot_registry:
+            response.success=False
+            response.message=f"{request.robot_id} already registered"
+            self.get_logger().warn(response.message)
+        else:
+            self.robot_registry[request.robot_id]={
+                "type":request.robot_type
+            }
+            response.success=True
+            response.message=f"{request.robot_id} registered successfully"
+            self.get_logger().info(response.message)
+        return response
+    
 
     def publish_status(self):
         self.get_logger().info("Publishing message: Fleet manager is availiable")
         message = String()
         message.data="Fleet manager is available"
-        self.publisher_.publish(message)
+        self.status_publisher_.publish(message)
 
 
 def main(args=None):
--- awfms_ws/src/awfms_fleet_manager/awfms_fleet_manager/robot_node.py
+++ awfms_ws/src/awfms_fleet_manager/awfms_fleet_manager/robot_node.py
@@ -2,23 +2,47 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import String
+from awfms_interfaces.srv import RegisterRobot
 
 class Robot(Node):
     def __init__(self):
         super().__init__("robot_1")
-        self.publisher_=self.create_publisher(String,"/robot/status",10)
+        self.status_publisher_=self.create_publisher(String,"/robot/status",10)
         self.timer_ = self.create_timer(0.5,self.publish_status)
+        self.register_client_=self.create_client(RegisterRobot,"/fleet_manager/register_robot")
         self.get_logger().info("Robot Node has been started")
 
     def publish_status(self):
         self.get_logger().info("Publishing message: Robot 1: IDLE")
         message=String()
         message.data="Robot 1: IDLE"
-        self.publisher_.publish(message)
+        self.status_publisher_.publish(message)
+
+    def register_robot(self,robot_id:str,robot_type:str):
+        while not self.register_client_.wait_for_service(timeout_sec=1.0): #wait for server to start, if not display the following
+            self.get_logger().warn("Waiting for service....")
+
+        request=RegisterRobot.Request()
+        request.robot_id=robot_id
+        request.robot_type=robot_type
+
+        future=self.register_client_.call_async(request) #asynchronous call used to avoid deadlock
+        future.add_done_callback(self.callback_register_robot)
+
+    def callback_register_robot(self,future):
+        response=future.result()
+        if response.success:
+            self.get_logger().info(f"Registration successfull: {response.message}")
+        else:
+            self.get_logger().info(f"Registration failed: {response.message}")
+
+
+
 
 def main(args=None):
     rclpy.init(args=args)
     node=Robot()
+    node.register_robot("robot_1","AGV")
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
